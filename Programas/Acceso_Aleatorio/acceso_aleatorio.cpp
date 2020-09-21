a8248436728298ec82d9c390a3873b315ae0f541
Miguel Rojas <miguel@Miguels-MacBook-Air.local>
2020-09-20 18:10:56 -0600

    Adicion de Programas restantes

--- Programas/Acceso_Aleatorio/acceso_aleatorio.cpp
+++ Programas/Acceso_Aleatorio/acceso_aleatorio.cpp
@@ -1,8 +1,9 @@
 #include <iostream> // acceso_aleratorio.cpp
 #include <fstream>
 #include <cstdlib>
-#include "Contacto.hpp"
+#include "Contacto.cpp"
 using namespace std;
+
 void escribir_o_leer(fstream &fcont, bool Escribir) {
     for (int i = 0; i < 6; ++i)
         if (Escribir) {
@@ -17,30 +18,30 @@ void escribir_o_leer(fstream &fcont, bool Escribir) {
 }
 
 void leer_el_3(fstream &fcont, Contacto &cont) {
- fcont.seekp(3*sizeof(Contacto));
- fcont.read(reinterpret_cast< char *>(&cont), sizeof(Contacto));
- cout << cont.obtenerId() << ":" << cont.obtenerNombre() << endl;
+    fcont.seekp(3*sizeof(Contacto));
+    fcont.read(reinterpret_cast< char *>(&cont), sizeof(Contacto));
+    cout << cont.obtenerId() << ":" << cont.obtenerNombre() << endl;
 }
 void escribir_el_3(fstream &fcont, Contacto &cont) {
- // INSERTAR CÓDIGO AQUÍ
- // INSERTAR CÓDIGO AQUÍ
- cont.establecerNombre("NOMBRE 5 -> 5000");
- fcont.write(reinterpret_cast<const char *>(&cont), sizeof(Contacto));
+    // INSERTAR CÓDIGO AQUÍ
+    // INSERTAR CÓDIGO AQUÍ
+    cont.establecerNombre("NOMBRE 5 -> 5000");
+    fcont.write(reinterpret_cast<const char*>(&cont), sizeof(Contacto));
 }
 
 void cambios(fstream &fcont) {
- Contacto cont;
- leer_el_3(fcont, cont);
- escribir_el_3(fcont, cont);
- leer_el_3(fcont, cont);
+    Contacto cont;
+    leer_el_3(fcont, cont);
+    escribir_el_3(fcont, cont);
+    leer_el_3(fcont, cont);
 }
 int main() {
- fstream fcont("contactos.dat", ios::in | ios::out | ios::binary );
- escribir_o_leer(fcont, true);
- cout << "ANTES:" << endl;
- escribir_o_leer(fcont, false);
- cout << endl << "CAMBIOS:" << endl;
- cambios(fcont);
- cout << endl << "DESPUES:" << endl;
- escribir_o_leer(fcont, false);
+    fstream fcont("contactos.dat", ios::in | ios::out | ios::binary );
+    escribir_o_leer(fcont, true);
+    cout << "ANTES:" << endl;
+    escribir_o_leer(fcont, false);
+    cout << endl << "CAMBIOS:" << endl;
+    cambios(fcont);
+    cout << endl << "DESPUES:" << endl;
+    escribir_o_leer(fcont, false);
 }
